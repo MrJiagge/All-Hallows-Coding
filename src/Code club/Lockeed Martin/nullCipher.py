c6f767e0e3e7201c9e08ea65eceb2c10a42d7eb8
Brandon Jiagge <bejiagge@gmail.com>
2024-02-01 22:15:33 +0000

    Create nullCipher.py

---
+++ src/Code club/Lockeed Martin/nullCipher.py
@@ -0,0 +1,20 @@
+vowels: list[str] = ["a", "e", "i", "o", "u"]
+ciphertext: str = input("Null Cipher Ciphertext: ")
+plaintextArr: list[str] = []
+
+def isAVowel(character: str) -> bool:
+    return character in vowels
+  
+
+
+def main() -> None:
+    for index in range(len(ciphertext) - 1):
+        char = ciphertext[index]
+        #if isAVowel(ciphertext[index - 1]):
+          #break
+        if isAVowel(char):
+            plaintextArr.extend([char, ciphertext[index + 1]])
+
+if __name__ == "__main__":
+    main()
+    print(plaintextArr)
